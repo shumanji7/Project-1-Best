4c64ab743034f0f5a63620faab38fc2c87b02390
mitesh18us <mitesh18us@hotmail.com>
2024-10-16 17:01:01 -0400

    timer

--- HTML/mitesh.js
+++ HTML/mitesh.js
@@ -1,81 +1,63 @@
-const right = document.getElementById('right');
-const wrong = document.getElementById('wrong');
-const wrong1 = document.getElementById('wrong1');
-const rightLabel = document.querySelector("label[for='right']");
-const wrongLabel = document.querySelector("label[for='wrong']");
-const wrong1Label = document.querySelector("label[for='wrong1']");
-const addBtn = document.querySelector('#button');
-const messageEl = document.querySelector('.message')
-const gifContainer = document.getElementById('gif-container');
+const btn = document.getElementById("checkAnswer");
+const resultEl = document.getElementById("grades");
+const timeEl = document.querySelector('#timer');
 
 
-right.addEventListener('change', event => {
+let secondsLeft = 60;
+let timerInterval;
 
-   if(event.target.checked) {
+  function setTime() {
+      timerInterval = setInterval(function () {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft + ' seconds remaining';
 
-    wrong.checked = false;
-    wrong1.checked = false;
-   
-  } 
-});
+      if (secondsLeft === 0) {
+        clearInterval(timerInterval);
+          window.location.href = "./Time_Up.html";
+      }      
+    }, 1000);
+  };
 
- wrong.addEventListener('change', event => {
+  setTime();
 
-     if(event.target.checked) {
-     
-    right.checked = false;
-    wrong1.checked = false;
-     }
+  btn.addEventListener("click", () => {
 
- });
+    const select1 = document.querySelector("input[name='q1']:checked");
+    const select2 = document.querySelector("input[name='q2']:checked");
+    const select3 = document.querySelector("input[name='q3']:checked");
+    const select4 = document.querySelector("input[name='q4']:checked");
+    const select5 = document.querySelector("input[name='q5']:checked");    
+            
+    let score = +select1.value + +select2.value + +select3.value + +select4.value + +select5.value;
+    let percentage = score/5*100;
+    let timeLeft = 60-secondsLeft;
 
- wrong1.addEventListener('change', event => {
-
-     if(event.target.checked) {
-     
-      right.checked = false;
-      wrong.checked = false;
-     }
-
- });
-
-
-addBtn.addEventListener('click', function () {
-    
-    if(right.checked) {
-        rightLabel.style.color = "green";
-        messageEl.textContent = "Your answer is right";
+    if (select1.value === "1") {
+          select1.parentElement.style.color = "#0add08"; 
+      } else {select1.parentElement.style.color = "#ff2400";   
+      }; 
         
+    if (select2.value === "1") {
+          select2.parentElement.style.color = "#0add08"; 
+      } else {select2.parentElement.style.color = "#ff2400";   
+      };
+
+    if (select3.value === "1") {
+          select3.parentElement.style.color = "#0add08"; 
+      } else {select3.parentElement.style.color = "#ff2400";   
+      };
+
+    if (select4.value === "1") {
+          select4.parentElement.style.color = "#0add08"; 
+      } else {select4.parentElement.style.color = "#ff2400";   
+      }; 
         
-        const gifImage = document.createElement('img');
-        gifImage.src = 'https://media1.tenor.com/m/c_na_nplHM4AAAAC/the-office-dwight-schrute.gif';
-        gifContainer.appendChild(gifImage);
+    if (select5.value === "1") {
+          select5.parentElement.style.color = "#0add08"; 
+      } else {select5.parentElement.style.color = "#ff2400";   
+      };
 
-        console.log('Right Answer');
-    } 
-    
-    if(wrong.checked) {
-        wrongLabel.style.color = "red";
-        messageEl.textContent = "Better luck next time";
-
-        const gifImage = document.createElement('img');
-        gifImage.src = 'https://media1.tenor.com/m/YsGxWABzct4AAAAd/death-stare-black-snake-moan.gif';
-        gifContainer.appendChild(gifImage);
+      clearInterval(timerInterval);
         
-        console.log('Wrong Answer');
-    }
-
-    if(wrong1.checked) {
-        wrong1Label.style.color = "red";
-        messageEl.textContent = "Better luck next time.";
-
-        const gifImage = document.createElement('img');
-        gifImage.src = 'https://media1.tenor.com/m/YsGxWABzct4AAAAd/death-stare-black-snake-moan.gif';
-        gifContainer.appendChild(gifImage);
-
-        console.log('Wrong Answer your score is `${score}`');
-    }
-    
-});
-  
-  
+      resultEl.textContent = `You got ${score} out of 5 right. Your score is ${percentage}.00%. you finished the test in ${timeLeft} seconds`;
+    });
